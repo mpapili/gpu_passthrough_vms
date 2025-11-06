6805b503562801fd12ddccf09764d553cc465f56
mpapili <michaelpapili@gmail.com>
2025-11-05 23:29:33 -0500

    split via IDs

--- force_soft_iommu_split.sh
+++ force_soft_iommu_split.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
+# NOTE - these were the ID's I found on my GPU's on my board, update them every time
 echo "applying iommu soft split.."
-sudo grubby --update-kernel=ALL --args="amd_iommu=on iommu=pt pcie_acs_override=downstream,multifunction"
+sudo grubby --update-kernel=ALL --args='pcie_acs_override=downstream,multifunction,ids=1022:1483,1002:1478,1002:1479'
--- unset_soft_iommu_split.sh
+++ unset_soft_iommu_split.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 echo "unsetting soft iommu split.."
-sudo grubby --update-kernel=ALL --remove-args="pcie_acs_override=downstream,multifunction"
+sudo grubby --update-kernel=ALL --remove-args="pcie_acs_override"
